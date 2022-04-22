176e3d8c28e3723ada4a850eeb11be1c610c28ae
Volvangor 2335444+volvangor@users.noreply.github.com <mMwzMh4q&AZ&-vrEdELjR2TY6kgEG!y>
2022-04-22 16:24:15 +1000

    add todos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #  OrderBookFeed
 #
 #  Written by James Hartman <JamesLouisHartman@gmail.com.au>
-#  Last modified 22/4/22, 2:35 pm
+#  Last modified 22/4/22, 4:24 pm
 #  Copyright (c) 2022 James Hartman. All Rights Reserved
 
 #  main.py
@@ -11,10 +11,18 @@
 #  main.py
 #  OrderBookFeed
 #
+#  main.py
+#  OrderBookFeed
+#
 
-# TODO: input stream parsing
+# done: input stream parsing
 # TODO: pass to def for messages
 # TODO: handle messages for add, update, delete and execute
+# TODO: keep track of order book, trigger print when change occurs in top n of buy or sell
+# TODO: do potential (possible) error handling
+# TODO: cleanup
+# TODO: optional: build for continuous input, i.e. unending, true stream of data
+# TODO: optional: stream corruption checking
 
 import sys
 
@@ -44,6 +52,7 @@ def parse_msg(mutable_bytes):
     Returns:
         dict
     """
+    #
 
 
 if __name__ == '__main__':
@@ -59,6 +68,7 @@ if __name__ == '__main__':
         seq_num = int.from_bytes(seq_bytes, byteorder = 'little', signed = False)
         msg_size = int.from_bytes(size_bytes, byteorder = 'little', signed = False)
         mutable_foo, msg_bytes = read_n_bytes(mutable_foo, msg_size)
-        print(f'{seq_num}, msg: {msg_bytes}')
+        parse_msg(msg_bytes)
+        print(f'seq: {seq_num}, size: {msg_size}, msg: {msg_bytes.hex(bytes_per_sep = 4, sep = " ")}')
 
 # note 1: this value could be larger, depending on the absolute minimum size of a message (theoretically zero)
